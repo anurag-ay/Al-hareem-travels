6bcb25127e201930e1392ec69ce1e402e738ff74
Mohd_Shahwaz <mohdshahwaz9305@gmail.com>
2023-06-01 11:54:34 +0530

    init commit

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@mui/material'
+
+function Header() {
+  return (
+<React.Fragment>
+    <AppBar>
+        <Toolbar>
+        <img src="" alt="logo" />
+        <Typography>Al Hareem Travels</Typography>
+        </Toolbar>
+    </AppBar>
+</React.Fragment>
+  )
+}
+
+export default Header
