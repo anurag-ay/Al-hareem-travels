eeae5cd4437866aa806ebecb014b76c0a1b40e33
anurag-ay <ay1909069@gmail.com>
2023-06-01 16:29:00 +0530

    implemented nav bar

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,42 +1,150 @@
+import { Link } from "react-router-dom";
 import React from "react";
-import './navbar.css';
-// import Logo  from '../images/Al_hareem_logo1.jpg'
-import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-function Navbar() {
+const drawerWidth = 240;
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        <Link to="/">
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+
+        <Link to="/about">
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary="About Us" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+
+        <Link to="/services">
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary="Services" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+
+        <Link to="/contact">
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary="Contact Us" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
-      <div 
-    className="navbar">
-      <Link className="link" to="/">
-        {/* <img className="logo" src={Logo} alt="" /> */}
-      </Link>
-        
-          <div className="project_name">
-          <Link className="link" to="/">
-            <div className="name">Al Hareem Travels</div>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <Link to="/">
+              <Button key="Home" sx={{ color: "#fff" }}>
+                Home
+              </Button>
             </Link>
-          </div>
-          <div className="item">
-          {
-            <ul>
-              <Link className="link" to="/">
-                <li><a>Home</a></li>
-              </Link>
-              <Link className="link" to="/about">
-            <li><a>About Us</a></li>
+            <Link to="/about">
+              <Button key="About Us" sx={{ color: "#fff" }}>
+                About Us
+              </Button>
             </Link>
-            <Link className="link" to="/services">
-               <li><a>Services</a></li>
+            <Link to="/services">
+              <Button key="Services" sx={{ color: "#fff" }}>
+                Services
+              </Button>
             </Link>
-            <Link className="link" to="/contact">
-              <li><a>Contact Us</a></li>
+            <Link to="/contact">
+              <Button key="Contact Us" sx={{ color: "#fff" }}>
+                Contact Us
+              </Button>
             </Link>
-            
-          </ul>
-          }
-          </div>
-    </div>    
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
   );
 }
 
-export default Navbar;
+DrawerAppBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default DrawerAppBar;
