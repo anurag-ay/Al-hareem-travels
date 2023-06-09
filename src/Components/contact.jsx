3345ecc33370ddf0a9936f87904074333bee7cce
anurag-ay <ay1909069@gmail.com>
2023-06-09 22:51:48 +0530

    improve contact componetnt

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -1,45 +1,90 @@
-import { Box, Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material'
-import React from 'react'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import styled from "styled-components";
 
 function contact() {
+  const Form = styled("form")({
+    display: "grid",
+    justifyContent: "center",
+  });
   return (
-     <Box id="contact">
-        <Typography
+    <Box id="contact">
+      <Typography
         variant="h4"
         component="h1"
         textAlign="center"
         sx={{ mt: "1em", mb: "1em" }}
       >
-       Contact Us
+        Contact Us
       </Typography>
-      <Card style={{maxWidth:"40rem" , margin:"2rem auto", padding:"10rem 5 rem"}}>
-        <CardContent>
-          <form >
-           <Grid container spacing={2}>
-            <Grid xs={12}  item>
-              <TextField label="First Name" placeholder='Enter Your first name' variant='outlined' fullWidth required />  
-            </Grid>
-            <Grid xs={12}  item>
-              <TextField label="Last Name" placeholder='Enter Your last name' variant='outlined' fullWidth required/>
-            </Grid>
-            <Grid item xs={12}>
-                <TextField type='email' label="Email" placeholder='Enter Your Email' variant='outlined' fullWidth required/>
-            </Grid>
-            <Grid item xs={12}>
-                <TextField type='number' label="Phone" placeholder='Enter Your Email' variant='outlined' fullWidth required/>
-            </Grid>
-              <Grid item xs={12}>
-                <TextField label="Message" multiline rows={4}  placeholder='Type your message here' variant='outlined' fullWidth required/>
-            </Grid>
-            <Grid item xs={12}>
-                  <Button type='submit' variant='contained' color='primary' sx={{justifyContent:"center"}}>Submit</Button>
-            </Grid>
-           </Grid>
-           </form>
-        </CardContent>
-      </Card>
-     </Box>
-  )
+      <Form>
+        <Grid container spacing={2} m="0" sx={{ maxWidth: "40em" }}>
+          <Grid item xs={12}>
+            <TextField
+              label="First Name"
+              placeholder="Enter Your first name"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Last Name"
+              placeholder="Enter Your last name"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              type="email"
+              label="Email"
+              placeholder="Enter Your Email"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              type="number"
+              label="Phone"
+              placeholder="Enter Your Email"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Message"
+              multiline
+              rows={4}
+              placeholder="Type your message here"
+              variant="outlined"
+              fullWidth
+              required
+            />
+          </Grid>
+        </Grid>
+        <Box p="1em" sx={{ display: "flex", justifyContent: "center" }}>
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </Box>
+      </Form>
+    </Box>
+  );
 }
 
-export default contact
+export default contact;
