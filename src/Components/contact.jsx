f5d34be17bd1b48e9ec11cfb31eace6d69fddea5
Mohd_Shahwaz <mohdshahwaz9305@gmail.com>
2023-06-09 20:49:02 +0530

    add contact

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -1,10 +1,44 @@
+import { Box, Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 function contact() {
   return (
-    <div>
-      contact
-    </div>
+     <Box id="contact">
+        <Typography
+        variant="h4"
+        component="h1"
+        textAlign="center"
+        sx={{ mt: "1em", mb: "1em" }}
+      >
+       Contact Us
+      </Typography>
+      <Card style={{maxWidth:"40rem" , margin:"2rem auto", padding:"10rem 5 rem"}}>
+        <CardContent>
+          <form >
+           <Grid container spacing={2}>
+            <Grid xs={12}  item>
+              <TextField label="First Name" placeholder='Enter Your first name' variant='outlined' fullWidth required />  
+            </Grid>
+            <Grid xs={12}  item>
+              <TextField label="Last Name" placeholder='Enter Your last name' variant='outlined' fullWidth required/>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField type='email' label="Email" placeholder='Enter Your Email' variant='outlined' fullWidth required/>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField type='number' label="Phone" placeholder='Enter Your Email' variant='outlined' fullWidth required/>
+            </Grid>
+              <Grid item xs={12}>
+                <TextField label="Message" multiline rows={4}  placeholder='Type your message here' variant='outlined' fullWidth required/>
+            </Grid>
+            <Grid item xs={12}>
+                  <Button type='submit' variant='contained' color='primary' sx={{justifyContent:"center"}}>Submit</Button>
+            </Grid>
+           </Grid>
+           </form>
+        </CardContent>
+      </Card>
+     </Box>
   )
 }
 
