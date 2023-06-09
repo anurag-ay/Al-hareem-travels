b6be8eebc64c6359892dd87f9c5f0e4dd88e741e
Mohd_Shahwaz <mohdshahwaz9305@gmail.com>
2023-06-09 23:52:00 +0530

    add divider in home

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -3,12 +3,16 @@ import Carousel from "./carousel";
 import Services from "../Components/services";
 import About from "./about";
 import Contact from "./contact";
+import { Divider } from "@mui/material";
 function home() {
   return (
     <>
       <Carousel />
+      <Divider/>
       <Services />
+      <Divider/>
       <About />
+      <Divider/>
       <Contact/>
     </>
   );
