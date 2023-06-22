132b9e671b81cacb7c8d09c0a775955c414d308e
anurag-ay <ay1909069@gmail.com>
2023-06-22 23:10:11 +0530

    add feat conditional rendering of sign in and sign up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import NavBar from "./Components/navbar";
 import Home from "./Components/home";
-import Contact from "./Components/contact";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Components/footer";
+import SignUp from "./Components/signup";
+import SignIn from "./Components/signin";
+import { useEffect, useState } from "react";
+import axios from "./api/axios";
 
 function App() {
+  const token = localStorage.getItem("auth-token");
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setphone] = useState("");
+  const [isValidToken, setIsValidToken] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      const verifyToken = async () => {
+        try {
+          const res = await axios.post("/auth", { auth_token: token });
+          if (res.status === 200) {
+            setIsValidToken(true);
+          }
+          const { firstName, lastName, email, phone } = res?.data;
+          setFirstName(firstName);
+          setLastName(lastName);
+          setphone(phone);
+          setEmail(email);
+        } catch (err) {
+          setIsValidToken(false);
+        }
+      };
+      verifyToken();
+    }
+    setIsValidToken(false);
+  }, [token]);
+
   return (
     <>
       <BrowserRouter>
-        <NavBar />
+        <NavBar
+          isValidToken={isValidToken}
+          firstName={firstName}
+          lastName={lastName}
+        />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-{/* 
-          <Route exact path="/contact" element={<Contact />} /> */}
+          <Route
+            path="/"
+            exact
+            element={
+              <Home
+                firstName={firstName}
+                lastName={lastName}
+                email={email}
+                phone={phone}
+              />
+            }
+          />
+          <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path="/signup" element={<SignUp />} />
         </Routes>
         <Footer />
       </BrowserRouter>
