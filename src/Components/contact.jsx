bab6478274ad94a374400b31ebdc14ea4b322dd2
anurag-ay <ay1909069@gmail.com>
2023-06-22 23:08:39 +0530

    connect contact to backend

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Alert,
+  Backdrop,
   Box,
   Button,
-  Collapse,
+  CircularProgress,
   Grid,
   Snackbar,
   TextField,
@@ -11,42 +12,93 @@ import {
 } from "@mui/material";
 import styled from "styled-components";
 import { MuiTelInput, matchIsValidTel } from "mui-tel-input";
+import axios from "../api/axios";
 
 const Form = styled("form")({
   display: "grid",
   justifyContent: "center",
 });
 
-function Contact() {
+function Contact(props) {
   const [firstName, setFirstName] = useState("");
-  const [lastName, setlastName] = useState("");
-  const [email, setemail] = useState("");
-  const [phoneNo, setphoneNo] = useState("");
-  const [message, setmessage] = useState("");
-  const [isValidPhoneNo, setisValidPhoneNO] = useState(true);
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+  const [isValidPhone, setIsValidPhone] = useState(true);
 
-  const [open, setOpen] = useState(false);
+  const [backDropOpen, setBackDropOpen] = useState(false);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [resMessage, setResMessage] = useState("");
+  const [resSuccess, setResSucess] = useState(false);
+
+  useEffect(() => {
+    const { firstName, lastName, email, phone } = props;
+    setFirstName(firstName);
+    setLastName(lastName);
+    setEmail(email);
+    setPhone(phone);
+  }, [props]);
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
+      setOpenSnackbar(false);
       return;
     }
 
-    setOpen(false);
+    setOpenSnackbar(false);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(e);
-    alert("Form submited");
-    setFirstName("");
-    setlastName("");
-    setemail("");
-    setphoneNo("");
-    setmessage("");
+    const formData = {
+      firstName,
+      lastName,
+      phone,
+      email,
+      message,
+    };
+    try {
+      const res = await axios.post("/message", formData);
+
+      if (res.status === 200) {
+        setBackDropOpen(false);
+        setResSucess(true);
+        setOpenSnackbar(true);
+        setResMessage("Message Sent Successfully");
+      }
+      setMessage("");
+    } catch (err) {
+      setResMessage(err.response.data);
+      setResSucess(false);
+      setBackDropOpen(false);
+      setOpenSnackbar(true);
+    }
   };
   return (
     <Box id="contact" sx={{ backgroundColor: "e5e5e5" }}>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={backDropOpen}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert
+          variant="filled"
+          severity={resSuccess ? "success" : "error"}
+          sx={{ width: "100%" }}
+          onClose={handleClose}
+        >
+          {resMessage}
+        </Alert>
+      </Snackbar>
       <Typography
         variant="h4"
         component="h1"
@@ -60,6 +112,7 @@ function Contact() {
           <Grid p="1em 0 1em 0" item xs={12}>
             <TextField
               label="First Name"
+              value={firstName}
               placeholder="Enter Your first name"
               variant="outlined"
               fullWidth
@@ -71,57 +124,60 @@ function Contact() {
           </Grid>
           <Grid p="1em 0 1em 0" item xs={12}>
             <TextField
+              value={lastName}
               label="Last Name"
               placeholder="Enter Your last name"
               variant="outlined"
               fullWidth
               // required
               onChange={(e) => {
-                setlastName(e.target.value);
+                setLastName(e.target.value);
               }}
             />
           </Grid>
           <Grid p="1em 0 1em 0" item xs={12}>
             <TextField
+              value={email}
               type="email"
               label="Email"
               placeholder="Enter Your Email"
               variant="outlined"
               fullWidth
               onChange={(e) => {
-                setemail(e.target.value);
+                setEmail(e.target.value);
               }}
               // required
             />
           </Grid>
           <Grid p="1em 0 0 0" item xs={12}>
             <MuiTelInput
-              value={phoneNo}
+              value={phone}
               label="Phone"
               defaultCountry="IN"
               fullWidth
               variant="outlined"
               sx={{ color: "#666674" }}
               onChange={(newPhone) => {
-                setphoneNo(newPhone);
-                setisValidPhoneNO(matchIsValidTel(newPhone));
+                setPhone(newPhone);
+                setIsValidPhone(matchIsValidTel(newPhone));
               }}
             />
           </Grid>
-          {isValidPhoneNo ? null : (
+          {isValidPhone ? null : (
             <Grid p="0 0 1em 0" item xs={12}>
               <Alert severity="error">Invalid Phone No.</Alert>
             </Grid>
           )}
           <Grid p="2em 0 1em 0" item xs={12}>
             <TextField
+              value={message}
               label="Message"
               multiline
               rows={4}
               placeholder="Type your message here"
               variant="outlined"
               onChange={(e) => {
-                setmessage(e.target.value);
+                setMessage(e.target.value);
               }}
               fullWidth
               // required
@@ -134,11 +190,6 @@ function Contact() {
           </Button>
         </Box>
       </Form>
-      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
-          This is a success message!
-        </Alert>
-      </Snackbar>
     </Box>
   );
 }
